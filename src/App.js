2eb6d0710fd88251ca2b903f4ea6b3978c48d2b0
Juraj Jarmek <badjjj@live.com>
2020-11-08 17:58:58 +0100

    Delete

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useState } from 'react';
-import { getTicketIds } from './services/tvApi';
-
-export const App = () => {
-  const [ticketIds, setTicketIds] = useState([]);
-
-  return <p>Hello</p>;
-};
