627db449dfc39b6fc5b62b78d03c49d025127f8d
Juraj Jarmek <badjjj@live.com>
2020-11-10 09:53:43 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,50 @@ const basicAuth = {
   password: pass,
 };
 
+/////////////////////////////////////////////////// Change
+
+const getTickets = (url) => {
+  return axios({
+    method: 'get',
+    url: url,
+    auth: basicAuth,
+  }).then((res) => res);
+};
+
+const logErr = (err) => {
+  if (err.res) {
+    console.log(err.res.data);
+    console.log(err.res.status);
+    console.log(err.res.headers);
+  } else if (err.req) {
+    console.log(err.req);
+  }
+  console.log('Error: ', err.message);
+  console.log(err.config);
+  console.log(err.stack);
+};
+
+const sendTickets = async (req, res) => {
+  try {
+    let res = await getTickets(baseUrl);
+    let tickets = res.data.tickets;
+    while (res.data.next_page) {
+      res = await getTickets(res.data.next_page);
+      tickets = tickets.concat(res.data.tickets);
+    }
+    tickets = tickets.reverse(); // Recent First
+    res.status(200).send(tickets);
+    console.log(
+      `Sent ${tickets.length} tickets to ${req.hostname} at ${Date()}.`
+    );
+  } catch (error) {
+    logErr(error);
+    res.status(error.res ? error.res.status : 500).send(`${error.message}`);
+  }
+};
+
+/////////////////////////////////////////////////// Change
+
 const App = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
